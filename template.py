dd3591e69aa05e424d8414bd6e9cf394c2bf75a2
abhishekanand-02 <bgsabhishekanand02@gmail.com>
2025-03-19 08:58:41 +0530

    Updated folder str

---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipelines/__init__.py",
+    "src/pipelines/training_pipeline.py",
+    "src/pipelines/prediction_pipeline.py",
+    "src/logger.py",
+    "src/exception.py",
+    "src/utils/utils.py",
+    "src/utils/__init__.py",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "experiment/experiments.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
